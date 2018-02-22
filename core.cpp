eb40323f7938a0ed33c45d2ec4b65e7cf9cf7b3f
Rational-pi <1salmieri.nicolas@gmail.com>
2018-02-22 11:12:34 +0100

    switch project code for a better base

--- core.cpp
+++ core.cpp
@@ -1,60 +1,108 @@
-#include <SDL2/SDL.h>
-#include <emscripten.h>
-#include <cstdlib>
-#include <iostream>
+#include <stdbool.h>
+#include <SDL.h>
+#include "SDL_opengl.h"
 
-struct context
+#ifdef EMSCRIPTEN
+#include <math.h>
+void gluPerspective(GLdouble fovy, GLdouble aspect, GLdouble zNear, GLdouble zFar)
 {
-    SDL_Renderer *renderer;
-    int iteration;
-};
-
-void mainloop(void *arg)
-{
-    SDL_Event ev;
-    SDL_WaitEvent(&ev);
-    std::cout << ev.cdevice.timestamp << std::endl;
-    context *ctx = static_cast<context*>(arg);
-    SDL_Renderer *renderer = ctx->renderer;
-    
-    // example: draw a moving rectangle
-    
-    // red background
-    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-    SDL_RenderClear(renderer);
-    
-    // moving blue rectangle
-    SDL_Rect r;
-    r.x = ctx->iteration % 255;
-    r.y = 50;
-    r.w = 50;
-    r.h = 50;
-    SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255 );
-    SDL_RenderFillRect(renderer, &r );
-
-    SDL_RenderPresent(renderer);
-
-    ctx->iteration++;
+    GLdouble xmin, xmax, ymin, ymax;
+    ymax = zNear * tan(fovy * M_PI / 360.0);
+    ymin = -ymax;
+    xmin = ymin * aspect;
+    xmax = ymax * aspect;
+    glFrustum(xmin, xmax, ymin, ymax, zNear, zFar);
 }
+#endif
+
+float width = 200, height = 200;
+float bpp = 0;
+float near = 10.0, far = 100000.0, fovy = 45.0;
+float position[3] = {0,0,-40};
+const float triangle[9] = {
+      0,  10, 0,  // top point
+    -10, -10, 0,  // bottom left
+     10, -10, 0   // bottom right
+};
+float rotate_degrees  = 90;
+float rotate_axis[3] = {0,1,0};
+
+SDL_Surface * screen = NULL;
+
+void setup()
+{
+    SDL_Init( SDL_INIT_VIDEO );
+
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1 );
+
+    screen = SDL_SetVideoMode(
+        width, height, bpp,
+        SDL_ANYFORMAT | SDL_OPENGL );
+
+    glViewport(0,0,width,height);
+
+    glPolygonMode( GL_FRONT, GL_FILL );
+    glPolygonMode( GL_BACK,  GL_LINE );
+
+    glMatrixMode(GL_PROJECTION);
+    gluPerspective(fovy,width/height,near,far);
+
+    glMatrixMode(GL_MODELVIEW);
+}
+
+void render()
+{
+    SDL_Event event;
+    while( SDL_PollEvent( &event ) )
+        if ( event.type == SDL_KEYDOWN ||
+             event.type == SDL_KEYUP )
+            SDL_Quit();
+
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glLoadIdentity();
+
+    glTranslatef(position[0],position[1],position[2]);
+
+    {
+        static Uint32 last = 0;
+
+        Uint32    now = SDL_GetTicks();
+        float   delta = (now - last) / 1000.0f; // in seconds
+                 last = now;
+
+        static float angle = 0;
+        angle += rotate_degrees * delta;
+
+        // c modulo operator only supports ints as arguments
+        #define MOD( n, d ) (n - (d * (int) ( n / d )))
+        angle = MOD( angle, 360 );
+
+        glRotatef( angle, rotate_axis[0], rotate_axis[1], rotate_axis[2] );
+    }
+
+    glBegin(GL_TRIANGLES);
+    for(int i=0; i<=6; i+=3)
+    {
+        glColor3f(i==0,i==3,i==6); // adding some color
+        glVertex3fv(&triangle[i]);
+    }
+    glEnd();
+
+    SDL_GL_SwapBuffers();
+}
+
+#ifdef EMSCRIPTEN
+#include <emscripten.h>
+#endif
 
 int main()
 {
-    SDL_Init(SDL_INIT_VIDEO);
-    SDL_Window *window;
-    SDL_Renderer *renderer;
-    SDL_CreateWindowAndRenderer(800, 600, 0, &window, &renderer);
-
-    context ctx;
-    ctx.renderer = renderer;
-    ctx.iteration = 0;
-
-    const int simulate_infinite_loop = 1; // call the function repeatedly
-    const int fps = -1; // call the function as fast as the browser wants to render (typically 60fps)
-    emscripten_set_main_loop_arg(mainloop, &ctx, fps, simulate_infinite_loop);
-    
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-
-    return EXIT_SUCCESS;
+    setup();
+#ifdef EMSCRIPTEN
+    emscripten_set_main_loop(render,60,true);
+#else
+    while (true)
+        render();
+#endif
 }
